38af75026b00c3bafad4fd2bef7ad60cc7dd0bf1
Marcelo Braga <matigela@gmail.com>
2021-06-12 16:25:30 -0300

    Consulta com vários critérios

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,4 +30,9 @@ public class PostService {
 		return repo.findByTitle(text);
 	}
 
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+	
 }
